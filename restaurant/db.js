daa9321ddb42451373c5cac68443aeeb71a16ae9
Vaidehi Bhandarkar <vaidehib23@Vaidehis-Laptop.local>
2025-06-25 11:19:09 +0530

    test updates

--- restaurant/db.js
+++ restaurant/db.js
@@ -1,28 +1,25 @@
 const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config({
+    path: path.resolve('/Users/vaidehib23/Desktop/BE/Node_L2/.env')
+});
+
 const URL = process.env.MONGO_URL;
 
 // const mongoURL = 'mongodb://localhost:27017/Restaurant';
 const mongoURL = URL;
+console.log("🔍 MONGO_URL =", process.env.MONGO_URL);
 
-mongoose.connect(mongoURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect(mongoURL)
+  .then(() => console.log('Connected to MongoDB server'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 const db = mongoose.connection;
 
-db.on ( 'connected', () => {
-    console.log( 'Connected to MongoDB server');
-});
-
-db. on ('error', (err) => {
-    console.error ('MongoDB connection error:', err);
-});
-
-db.on ('disconnected', () => {
-    console.log('MongoDB disconnected');
+db.on('disconnected', () => {
+  console.log('MongoDB disconnected');
 });
 
 module.exports = {
-    db,
-}
+  db,
+};
